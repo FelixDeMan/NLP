a75883f0fa10543c33bb551784ee53a3aa8a240d
FelixDeMan <deman.janfelix@gmail.com>
2022-04-13 16:46:28 +0200

    Eval & Baseline AF! baselines (length & freq) nog tunen en dan evaluaten!

--- TODO_baselines.py
+++ TODO_baselines.py
@@ -3,35 +3,166 @@
 # Random baseline: randomly assigns one of the classes. Make sure to set a random seed and average the accuracy over 100 runs.
 # Length baseline: determines the class based on a length threshold
 # Frequency baseline: determines the class based on a frequency threshold
-
+import random
+from statistics import mean
+import numpy as np
+from collections import Counter
 from model.data_loader import DataLoader
 
 # Each baseline returns predictions for the test data. The length and frequency baselines determine a threshold using the development data.
 
 def majority_baseline(train_sentences, train_labels, testinput, testlabels):
-    predictions = []
 
-    # TODO: determine the majority class based on the training data
-    # ...
-    majority_class = "X"
+     # TODO: determine the majority class based on the training data
+     # ...
+    print((train_labels[0]))
+    counterN = 0
+    counterC = 0
+    for labels in train_labels:
+        counterN += labels.count("N")
+        counterC += labels.count("C")
+    if counterN > counterC:
+        majority_class = "N"
+    if counterC > counterN:
+        majority_class = "C"
+
     predictions = []
+    instances = []
+
+
     for instance in testinput:
+        print(instance)
         tokens = instance.split(" ")
         instance_predictions = [majority_class for t in tokens]
-        predictions.append(instance, instance_predictions)
+        predictions.append(instance_predictions)
+        instances.append(instance)
+        print(instance_predictions)
 
     # TODO: calculate accuracy for the test input
     # ...
+    labelcountN = 0
+    labelcountC = 0
+
+    for labels in testlabels:
+        labelcountN += labels.count("N")
+        labelcountC += labels.count("C")
+
+    if majority_class == "C":
+        accuracy = labelcountC / (labelcountN + labelcountC)
+
+    if majority_class == "N":
+        accuracy = labelcountN / (labelcountN + labelcountC)
+
+    return accuracy, predictions
+
+
+def random_baseline(train_sentences, train_labels, testinput, testlabels, B = 100):
+    np.random.seed(2020)
+    predictions = []
+    instances = []
+    correct = []
+    accuracy = []
+    for b in range(B):
+        for i, instance in enumerate(testinput):
+            print(instance)
+            tokens = instance.split(" ")
+            predictions.append(instance_predictions)
+
+            for j, label in enumerate(testlabels[i].split(' ')):
+                if label == instance_predictions[j]: correct.append(1)
+                else: correct.append(0)
+            instances.append(instance)
+            print(instance_predictions)
+        accuracy.append(sum(correct) / len(correct))
+
+
+
+    print(accuracy)
+    accuracy = mean(accuracy)
+    print(accuracy)
     return accuracy, predictions
 
 
 
+def freq_baseline(train_sentences, train_labels, testinput, testlabels, treshold = 3):
+    predictions = []
+    instances = []
+    correct = []
+
+    word_frequencies = Counter()
+
+    for sentence in train_sentences:
+        words = []
+        for token in sentence:
+            words.append(token)
+        word_frequencies.update(words)
+
+    print(word_frequencies)
+    for i, instance in enumerate(testinput):
+        print(instance)
+        tokens = instance.split(" ")
+        instance_predictions = ["N" if word_frequencies[t] > treshold else "C" for t in tokens]
+        predictions.append(instance_predictions)
+        instances.append(instance)
+        print(instance_predictions)
+        print(testlabels[i])
+
+        for j, label in enumerate(testlabels[i].split(" ")):
+            if label == instance_predictions[j]: correct.append(1)
+            else: correct.append(0)
+
+    accuracy = sum(correct) / len(correct)
+
+    return accuracy, predictions
+
+def length_baseline(train_sentences, train_labels, testinput, testlabels, treshold = 3):
+    predictions = []
+    instances = []
+    correct = []
+    for i, instance in enumerate(testinput):
+        print(instance)
+        tokens = instance.split(" ")
+        instance_predictions = ["C" if len(t) > treshold else "N" for t in tokens]
+        predictions.append(instance_predictions)
+        instances.append(instance)
+        print(instance_predictions)
+        print(testlabels[i])
+
+        for j, label in enumerate(testlabels[i].split(" ")):
+            if label == instance_predictions[j]: correct.append(1)
+            else: correct.append(0)
+
+    accuracy = sum(correct) / len(correct)
+
+    return accuracy, predictions
+
+
+# TODO: output the predictions in a suitable way so that you can evaluate them
+def write_output(outfile, predictions, labels, input):
+    labels_list = []
+    print("Labels = ", labels)
+    for labs in labels:
+
+        x = labs.split(" ")
+        for label in x: label.strip()
+        #print(x[-1])
+        #if "\n" in x[-1]: x[-1].replace("\n", "")
+        labels_list.append(x)
+    print(labels_list)
+    with open(outfile, "w") as f:
+        for i, sentences in enumerate(input):
+            tokens = sentences.split(" ")
+            for j, token in enumerate(tokens):
+                #print([token, labels[i][j], predictions[i][j]])
+                f.write("\t".join([token.strip(), labels_list[i][j].strip(), predictions[i][j]]))
+                f.write("\n")
+
 
 
 if __name__ == '__main__':
-    train_path = "data/preprocessed/train"
-    dev_path = "data/preprocessed/dev"
-    test_path = "data/preprocessed/test"
+    train_path = "data/preprocessed/train/"
+    dev_path = "data/preprocessed/val/"
+    test_path = "data/preprocessed/test/"
 
     # Note: this loads all instances into memory. If you work with bigger files in the future, use an iterator instead.
 
@@ -50,8 +181,10 @@ if __name__ == '__main__':
     with open(test_path + "sentences.txt") as testfile:
         testinput = testfile.readlines()
 
-    with open("test_path + labels.txt") as test_labelfile:
+    with open(test_path + "labels.txt") as test_label_file:
         testlabels = test_label_file.readlines()
-    majority_accuracy, majority_predictions = majority_baseline(train_sentences, train_labels, testinput)
 
-    # TODO: output the predictions in a suitable way so that you can evaluate them
+    majority_accuracy, majority_predictions = length_baseline(train_sentences, train_labels, testinput, testlabels, treshold= 5)
+    write_output("length_baseline.tsv", majority_predictions, testlabels, testinput)
+    print(majority_accuracy)
+
--- TODO_detailed_evaluation.py
+++ TODO_detailed_evaluation.py
@@ -1,2 +1,68 @@
 # The original code only outputs the accuracy and the loss.
 # Process the file model_output.tsv and calculate precision, recall, and F1 for each class
+import pandas as pd
+
+
+
+def clean_data():
+    model_output = pd.read_csv("experiments/base_model/model_output.tsv", sep="\t")
+    model_output.columns=["word", "label", "prediction"]
+    #print(model_output)
+    model_output = model_output.dropna()
+    print(model_output.head())
+    #model_output.rename(columns={"word", "label", "prediction"}, inplace=True)
+    #print(model_output)
+    model_output.to_csv("model_out.csv")
+
+
+if __name__ == '__main__':
+
+    #Per class, per model: Precision, Recall, F1
+    #clean_data()
+    model_output = pd.read_csv("model_out.csv")
+
+    C = model_output[model_output["label"] == "C"]
+    print(C)
+   # C.to_csv("c.tsv")
+    N = model_output[model_output["label"] == "N"]
+    #N.to_csv("n.tsv")
+    print(N)
+
+    #Precision = True Positives / (True Positives + False Positives)
+
+    trueposC = C[C["label"] == C["prediction"]].shape[0]
+    falseposC =  N[N["prediction"] == "C"].shape[0]
+
+    trueposN = N[N["label"] == N["prediction"]].shape[0]
+    falseposN = C[C["prediction"] == "N"].shape[0]
+    print("WHAT", trueposC)
+    print("Falsepos", falseposC)
+
+    precisionC =  trueposC / (trueposC + falseposC)
+    print(precisionC)
+
+    precisionN = trueposN / (trueposN + falseposN)
+    print(trueposN, falseposN, precisionN)
+
+
+    #Recall = True Positives / (True Positives + False Negatives)
+    falsenegC = C[C["prediction"] == "N"].shape[0]
+    recallC = trueposC / (trueposC + falsenegC)
+    print(trueposC, falsenegC, recallC)
+
+    falsenegN = N[N["prediction"] == "C" ].shape[0]
+    recallN = trueposN / (trueposN + falsenegN)
+    print(trueposN, falsenegN, recallN)
+
+     #F1 = 2 * (Precision * Recall) / (Precision + Recall)
+    F1C = 2 * (precisionC * recallC) / (precisionC + recallC)
+    #
+    F1N = 2 * (precisionN * recallN) / (precisionN + recallN)
+    #
+    #
+    print("Precision C = ", precisionC,  "  N = ", precisionN)
+    print("Recall C = ", recallC, " N = ", recallN)
+    print(F1C, F1N)
+
+    ## TO - DO weighthed average
+
